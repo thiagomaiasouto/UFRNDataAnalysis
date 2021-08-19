e68d2d30119c3513262045358dfccbcc9f51ef32
thiagomaiasouto <52938321+thiagomaiasouto@users.noreply.github.com>
2021-08-19 14:20:21 -0300

    update main_app.py
    New release for deployment

--- main_app.py
+++ main_app.py
@@ -1,20 +1,23 @@
-# Bilbiotecas necessÃ¡rias
-
+# -*- coding: utf-8 -*-
+import streamlit as st
 import numpy as np
 import pandas as pd
-import streamlit as st
+import gdown as gd
 import re
-import plotly.graph_objects as go
+import plotly.graph_objs as go
 import plotly.express as px
+from plotly.subplots import make_subplots
+
+
 
 @st.cache(suppress_st_warning=True)
-def load_dataframe(file_name):
-  df = pd.read_csv(file_name,              # nome do arquivo .csv que serÃ¡ carregado
-                  delimiter=';',           # esses arquivos .csv possuem os dados serparados por ponto e vÃ­rgula (semicolon)
-                  decimal=",",             # esses arquivos .csv possuem o ponto decimal como uma vÃ­rgula, necessitando trocar para o ponto decimal
-                  encoding='utf-8',        # a codificaÃ§Ã£o serÃ¡ em utf-8
-                  skiprows=0,              # skip default rows depending in dataframe configuration header
-                  )
+def load_dataframe():
+  
+  url = 'https://drive.google.com/file/d/1-34MNU3QuKplfPI-_BqDXx7SeZoAzEEE/view?usp=sharing'
+  output = 'df.csv'
+  gd.download(url,output)
+  df = pd.read_csv('df.csv', sep=';')
+  
   return df
 
 def coordinates_to_horary(x,y):
@@ -59,88 +62,118 @@ def extract_numeric_period(list_):
             if match: array_ = map_period_tuple(match.groups(), array_) # se identificar padrÃ£o anterior, deverÃ¡ converte-lo em matriz
         return array_
 
-df_matriculas = {'2018.1': pd.DataFrame(), '2018.2': pd.DataFrame(), '2019.1': pd.DataFrame(), '2019.2': pd.DataFrame()}
-df_matriculas['2018.1'] = load_dataframe('matricula-componente-20181.csv')
-df_matriculas['2018.2'] = load_dataframe('matricula-componente-20182.csv')
-df_matriculas['2019.1'] = load_dataframe('matricula-componente-20191.csv')
-df_matriculas['2019.2'] = load_dataframe('matricula-componente-20192.csv')
 
-df_turmas = {'2018.1': pd.DataFrame(), '2018.2': pd.DataFrame(), '2019.1': pd.DataFrame(), '2019.2': pd.DataFrame()}
-df_turmas['2018.1'] = load_dataframe('turmas-2018.1.csv')
-df_turmas['2018.2'] = load_dataframe('turmas-2018.2.csv')
-df_turmas['2019.1'] = load_dataframe('turmas-2019.1.csv')
-df_turmas['2019.2'] = load_dataframe('turmas-2019.2.csv')
+df_final = load_dataframe('df.csv')
 
-df_componentes = load_dataframe('componentes-curriculares-presenciais.csv')
+st.session_state.caminho = st.sidebar.selectbox('Navegar:',['Monitoria','Analise'])
 
-semestres = list(df_turmas.keys())
+if st.session_state.caminho == 'Monitoria':
+  df_test = df_final.filter(['semestre','unidade_responsavel','disciplina','descricao_horario', 'discente'])
 
-colunas_turmas = ['id_turma', 'id_componente_curricular', 'descricao_horario']
-colunas_componentes = ['id_componente', 'unidade_responsavel', 'nome']
-colunas_matriculas = ['id_turma', 'discente']
+  st.title("Melhor Horário para Monitoria")
+  with st.sidebar:
+      st.title("Filtros")
+      semestre = st.selectbox('Semestre:',df_test['semestre'].sort_values().unique())
+      df_test = df_test[df_test['semestre']==semestre]
+      unidade = st.selectbox('Unidade Responsável:',df_test.unidade_responsavel.sort_values().unique())
+      disciplinas = st.multiselect('Disciplinas:',df_test[df_test['unidade_responsavel']==unidade].disciplina.sort_values().unique())
 
-df_componentes = df_componentes.filter(colunas_componentes).reset_index() # manter colunas essenciais
+  if(disciplinas):
+      discentes = df_test[df_test['disciplina'].isin(disciplinas)].discente
+      horarios = df_test[df_test['discente'].isin(discentes)].drop_duplicates().filter(['descricao_horario','discente']).groupby(['descricao_horario']).count().rename(columns={"discente": "peso"}).reset_index()
+      
+      horarios_modificando = horarios['descricao_horario'].str.replace("[(@*&?].*[)@*&?]", "", regex=True) # remover conteÃºdo entre parenthesis das strings e.g: 4T456 (22/07/2019 - 07/12/2019) -> 4T456 
+      horarios_listas = horarios_modificando.str.split(' ').dropna() # dividir string '6T23456 7T123456 6N12345' em lista [6T23456, 7T123456, 6N12345]
+      horarios_matrix = horarios_listas.apply(extract_numeric_period) # aplicar conversÃ£o de horÃ¡rio formato UFRN para matriz
+      horarios_matrix_pesos = horarios_matrix*horarios['peso'] # aplicando os pesos aos horÃ¡rios existentes
 
-for semestre in semestres:
-  df_matriculas[semestre] = df_matriculas[semestre].filter(colunas_matriculas).reset_index() # manter colunas essenciais
-  df_turmas[semestre] = df_turmas[semestre].filter(colunas_turmas).reset_index() # manter colunas essenciais
-  df_turmas[semestre] = df_turmas[semestre].dropna(subset=['descricao_horario']) # descricao_horario deve ser nÃ£o nula, qualquer entrada NaN deve ser descartada                   
+      piores_horarios_monitoria = horarios_matrix_pesos.sum() # quanto maior o valor, mais discentes (dos escolhidos) estÃ£o ocupados
+      total_discentes_selecionados = np.max(piores_horarios_monitoria)
+      melhores_horarios_monitoria = total_discentes_selecionados-piores_horarios_monitoria # quanto maior o valor, mais discentes (dos escolhidos) estÃ£o desocupados
 
-columns_to_drop = ['id_componente_curricular', 'id_componente']
-for semestre in semestres: 
-  df_turmas[semestre] = pd.merge(df_turmas[semestre], df_componentes, 
-                                 how='inner', left_on='id_componente_curricular',
-                                 right_on='id_componente').drop(columns_to_drop, axis=1) # nomeando turmas com base na sua ID de componente curricular
-  df_turmas[semestre] = df_turmas[semestre].rename(columns= {'nome': 'disciplina'}, inplace=False) # renomear coluna para que fique claro o nome da disciplina
-st.title("Melhor HorÃ¡rio para Monitoria")
-with st.sidebar:
-    st.title("Filtros")
-    semestre_dd = st.selectbox('Semestre:',semestres)
-    unidade_dd = st.selectbox('Unidade ResponsÃ¡vel:',df_turmas[semestre]['unidade_responsavel'].sort_values().unique())
-    disciplinas = st.multiselect('Disciplinas:',df_turmas[semestre]['disciplina'][df_turmas[semestre]['unidade_responsavel']==unidade_dd].sort_values().unique())
+      labels_dia_semana = ['SEG','TER','QUA','QUI','SEX','SAB']
+      labels_horario = ['M1', 'M2', 'M3', 'M4', 'M5', 'M6', 
+                      'T1', 'T2', 'T3', 'T4', 'T5', 'T6', 
+                      'N1', 'N2', 'N3', 'N4']
+      labels_horario_r = ['N4', 'N3', 'N2', 'N1', 'T6', 'T5', 
+                      'T4', 'T3', 'T2', 'T1', 'M6', 'M5', 
+                      'M4', 'M3', 'M2', 'M1']                    
+      colorscale = [[0, 'red'], [1, 'green']]
+      
+      fig = go.Figure(data=go.Heatmap(
+                    z=np.flipud(melhores_horarios_monitoria),
+                    x=labels_dia_semana,
+                    y=labels_horario_r,
+                    hoverongaps = False,
+                    hovertemplate = "Dia:%{x}<br>HorÃ¡rio:%{y}<br>Alunos Livres:%{z}",
+                    colorscale='darkmint'))
+      st.markdown("## HorÃ¡rios Livres")
+      st.write(fig)
 
-if(disciplinas):
-    id_turmas_selecionadas = list(df_turmas[semestre][df_turmas[semestre]['disciplina'].isin(disciplinas)].id_turma) # id das turmas referentes Ã s disciplinas selecionadas
-    discentes_selecionados = df_matriculas[semestre][(df_matriculas[semestre].id_turma).isin(id_turmas_selecionadas)]['discente'].unique() # discentes que estÃ£o matriculados nas turmas buscadas
-    total_discentes_selecionados = discentes_selecionados.size # quantidade de discentes_selecionados 
-    turmas_discentes_selecionados = df_matriculas[semestre][df_matriculas[semestre]['discente'].isin(discentes_selecionados)].drop_duplicates().groupby(['id_turma'])['discente'].describe()[['count']].reset_index() # todas as turmas que os discentes selecionados estÃ£o matriculados
+      st.markdown("## HorÃ¡rios x Alunos Livres")
 
-    horarios = df_turmas[semestre][df_turmas[semestre]['id_turma'].isin(turmas_discentes_selecionados['id_turma'])][['descricao_horario','id_turma']].reset_index()
-    horarios = horarios.join(turmas_discentes_selecionados.set_index('id_turma'),on='id_turma').drop(columns=['index', 'id_turma']).rename(columns={"count": "peso"})
+      dia_dd = st.selectbox('Dia:',labels_dia_semana)
+      dia_arr = [row[labels_dia_semana.index(dia_dd)] for row in melhores_horarios_monitoria]
+      maxDia = max(dia_arr)
+      fig = px.bar(pd.DataFrame ({'HorÃ¡rios':labels_horario,'Alunos Livres':dia_arr}, columns = ['HorÃ¡rios','Alunos Livres']), x='HorÃ¡rios', y='Alunos Livres', color_discrete_sequence =['darkseagreen']*maxDia)
+      
+      st.write(fig)
+elif st.session_state.caminho == 'Analise':
+  st.title('Analise')
+  
+  with st.sidebar:
+      st.title("Filtros")
+      
+      df_test = df_final.filter(['semestre','nome_docente','disciplina','descricao_horario', 'discente'])
+      
+      semestre_sel = st.selectbox('Semestre:',df_test['semestre'].sort_values().unique())
+      
+      df_test = df_test[df_test['semestre']==semestre_sel]
+      
+      docente_sel = st.selectbox('Docente:',df_test.nome_docente.sort_values().unique())
+      
+      disciplina_sel = st.selectbox('Disciplina:',df_test[df_test['nome_docente']==docente_sel].disciplina.sort_values().unique())
+      
+      df_query = df_final.query('disciplina == @disciplina_sel & nome_docente == @docente_sel & semestre == @semestre_sel ')
+     
+  fig = px.histogram(data_frame = df_query, 
+                   x = 'nota', nbins=10, color='unidade', 
+                   title = 'Distribuição de notas na matéria de ' + disciplina_sel + " em cada unidade", 
+                   hover_data= ['nota'], barmode = 'overlay', opacity = 0.75)
+  
+  st.plotly_chart(fig)
+     
+  #st.header("Novo grafico")
+  
+  df_query_move = df_final[(df_final['disciplina'] == disciplina_sel) & (df_final['nome_docente'] == docente_sel)]
+  
+  fig2 = px.scatter(df_query_move , x= 'media_final',  animation_frame= 'semestre', animation_group='discente',
+           size='media_final', hover_name='media_final')
 
-    horarios_modificando = horarios['descricao_horario'].str.replace("[(@*&?].*[)@*&?]", "", regex=True) # remover conteÃºdo entre parenthesis das strings e.g: 4T456 (22/07/2019 - 07/12/2019) -> 4T456 
-    horarios_listas = horarios_modificando.str.split(' ').dropna() # dividir string '6T23456 7T123456 6N12345' em lista [6T23456, 7T123456, 6N12345]
-    horarios_matrix = horarios_listas.apply(extract_numeric_period) # aplicar conversÃ£o de horÃ¡rio formato UFRN para matriz
-    horarios_matrix_pesos = horarios_matrix*horarios['peso'] # aplicando os pesos aos horÃ¡rios existentes
+  fig2.update_layout(title = 'Distribuições da média final de cada discente por semestre')
+  
+  st.plotly_chart(fig2)
+  
+  notas_medias = df_final.groupby(by = ['id_turma', 'nome_docente'])['media_final'].mean().reset_index()
+  notas_medias = notas_medias.drop('id_turma', axis = 1)
+ 
+  notas_avaliacao = df_final.groupby(by = ['nome_docente'])[['atuacao_profissional_media','postura_profissional_media']].mean().reset_index()
+  df_media_avaliacao = notas_medias.merge(notas_avaliacao, how= 'inner', on = 'nome_docente')
+  df_media_avaliacao = df_media_avaliacao.groupby('nome_docente')[['media_final','atuacao_profissional_media','postura_profissional_media']].mean().reset_index()
+  m_nota_formacao_corr = df_media_avaliacao.corr(method = 'pearson')
 
-    piores_horarios_monitoria = horarios_matrix_pesos.sum() # quanto maior o valor, mais discentes (dos escolhidos) estÃ£o ocupados
-    total_discentes_selecionados = np.max(piores_horarios_monitoria)
-    melhores_horarios_monitoria = total_discentes_selecionados-piores_horarios_monitoria # quanto maior o valor, mais discentes (dos escolhidos) estÃ£o desocupados
+  fig3 = go.Figure(data=go.Heatmap(
+                   z=m_nota_formacao_corr.values.astype('float64'),
+                   x = ['media_final', 'atuacao_profissional_media' ,'postura_profissional_media'],
+                   y = ['media_final', 'atuacao_profissional_media' ,'postura_profissional_media'],
+                   hoverongaps = False, colorscale='Viridis'))
+  fig3.update_xaxes(side="top")
+  fig3.update_layout(title = 'Correlação entre média geral de alunos de todas as turmas com as avaliações dos docentes')
+  st.markdown('## Correlacao')
+  st.plotly_chart(fig3)
 
-    labels_dia_semana = ['SEG','TER','QUA','QUI','SEX','SAB']
-    labels_horario = ['M1', 'M2', 'M3', 'M4', 'M5', 'M6', 
-                    'T1', 'T2', 'T3', 'T4', 'T5', 'T6', 
-                    'N1', 'N2', 'N3', 'N4']
-    labels_horario_r = ['N4', 'N3', 'N2', 'N1', 'T6', 'T5', 
-                    'T4', 'T3', 'T2', 'T1', 'M6', 'M5', 
-                    'M4', 'M3', 'M2', 'M1']                    
-    colorscale = [[0, 'red'], [1, 'green']]
-    
-    fig = go.Figure(data=go.Heatmap(
-                   z=np.flipud(melhores_horarios_monitoria),
-                   x=labels_dia_semana,
-                   y=labels_horario_r,
-                   hoverongaps = False,
-                   hovertemplate = "Dia:%{x}<br>HorÃ¡rio:%{y}<br>Alunos Livres:%{z}",
-                   colorscale='darkmint'))
-    st.markdown("## HorÃ¡rios Livres")
-    st.write(fig)
-
-    st.markdown("## HorÃ¡rios x Alunos Livres")
-
-    dia_dd = st.selectbox('Dia:',labels_dia_semana)
-    dia_arr = [row[labels_dia_semana.index(dia_dd)] for row in melhores_horarios_monitoria]
-    maxDia = max(dia_arr)
-    fig = px.bar(pd.DataFrame ({'HorÃ¡rios':labels_horario,'Alunos Livres':dia_arr}, columns = ['HorÃ¡rios','Alunos Livres']), x='HorÃ¡rios', y='Alunos Livres', color_discrete_sequence =['darkseagreen']*maxDia)
-    
-    st.write(fig)
+  
+  
+  
+else:
+  st.title('Nada')
